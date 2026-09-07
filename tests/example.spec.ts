91728c163a054694b97eb1f51e168eff238f3354
Zolbo <zolbinzolbo@gmail.com>
2026-09-07 22:19:44 +0800

    Амжилттай тестүүдийн дараа logout нэмсэн

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,8 +5,8 @@ test('1. Амжилттай нэвтрэх', async ({ page }) => {
 
   await page.getByPlaceholder('Username').fill('standard_user');
   await page.getByPlaceholder('Password').fill('secret_sauce');
-  await page.getByRole('button', { name: 'Login' }).click();
-
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
   await expect(page.getByText('Products')).toBeVisible();
 });
 
